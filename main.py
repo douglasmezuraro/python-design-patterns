6fdbdf8ae2e32d94a2ad5b18e11535f63af6d5c8
Douglas Mezuraro <douglasmez@gmail.com>
2022-01-29 18:28:12 -0300

    Implementado singleton usando o conceito de metaclasses

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from singleton import Singleton, LazySingleton, MonoStateSingleton
+from singleton import Singleton, LazySingleton, MonoStateSingleton, MetaClassSingletonExample
 
 if __name__ == '__main__':
     assert(id(Singleton()) == id(Singleton()))
     assert(id(LazySingleton.get_instance()) == id(LazySingleton.get_instance()))
+    assert(id(MetaClassSingletonExample()) == id(MetaClassSingletonExample()))
 
     x, y = MonoStateSingleton(), MonoStateSingleton()
     x.foo = 'bar'
--- singleton.py
+++ singleton.py
@@ -2,7 +2,7 @@ from typing import Any, Mapping, Iterable
 
 class Singleton(object):
 
-    def __new__(cls):
+    def __new__(cls) -> 'Singleton':
         if not hasattr(cls, 'instance'):
             cls.instance = super(Singleton, cls).__new__(cls)
 
@@ -14,7 +14,7 @@ class LazySingleton(object):
     __instance = None
 
     @classmethod
-    def get_instance(cls):
+    def get_instance(cls) -> 'LazySingleton':
         if not cls.__instance:
             cls.__instance = LazySingleton()
 
@@ -25,8 +25,23 @@ class MonoStateSingleton(object):
 
     __state = {}
 
-    def __new__(cls, *args: Iterable[Any], **kwargs: Mapping[str, Any]):
+    def __new__(cls, *args: Iterable[Any], **kwargs: Mapping[str, Any]) -> 'MonoStateSingleton':
         obj = super(MonoStateSingleton, cls).__new__(cls, *args, **kwargs)
         obj.__dict__ = cls.__state
 
         return obj
+
+
+class MetaClassSingleton(type):
+
+    __instances = {}
+
+    def __call__(cls, *args: Iterable[Any], **kwargs: Mapping[str, Any]) -> Any:
+        if cls not in cls.__instances:
+            cls.__instances[cls] = super(MetaClassSingleton, cls).__call__(*args, **kwargs)
+
+        return cls.__instances[cls]
+
+
+class MetaClassSingletonExample(metaclass=MetaClassSingleton):
+    pass
